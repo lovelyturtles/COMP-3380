8534ffbd332df05f56990f26c54d63ca257a9316
Agape Seo <96143174+gapy04@users.noreply.github.com>
2021-12-23 18:12:32 -0600

    Update MyDatabase.java

--- MyDatabase.java
+++ MyDatabase.java
@@ -102,10 +102,10 @@ public class MyDatabase
 
     public void OccupationActions()
     {
-        //prints the table to see which quest giver has the most variety in the types of quests they give
+        //prints the table to see which occupation has the most actions
         try
         {
-            String myQuery = "Select OccupationName, count( ActionName ) As NumberOfActions From Perform Group By OccupationName Order By NumberOfActions Desc;";
+            String myQuery = "Select Occupations.OccupationName, count( ActionName ) As NumberOfActions From Occupations join Perform on Occupations.OccupationName = Perform.OccupationName Group By Occupations.OccupationName Order By NumberOfActions DESC;";
             PreparedStatement myStatement = this.connection.prepareStatement(myQuery);
             ResultSet myResult = myStatement.executeQuery();
             PrintStream resultPrinter = System.out;
@@ -120,7 +120,7 @@ public class MyDatabase
 
     public void OccupationQuests()
     {
-        //prints the table to see which quest giver has the most variety in the types of quests they give
+        //prints the table to see which occupation has the most quests
         try
         {
             String myQuery = "Select OccupationName, count( questID ) As NumberOfQuests From Occupations join Quests on Occupations.questID = Quests.questID Group By OccupationName Order By NumberOfQuests Desc;";
@@ -135,7 +135,155 @@ public class MyDatabase
             System.out.println("Error in getting Occupation - Quests table.");
         }
     }
+	
+    public void SharedActions()
+    {
+        //Gives the table with the actions that is the most shared among occupations
+        try
+        {
+            String myQuery = "Select ActionName, Count(OccupationName) As SharedAmong From Perform Group By ActionName Order By SharedAmong Desc;";
+            PreparedStatement myStatement = this.connection.prepareStatement(myQuery);
+            ResultSet myResult = myStatement.executeQuery();
+            PrintStream resultPrinter = System.out;
+            String queryResult = myResult.getString(1);
+            resultPrinter.println("The action that is shared by the most occupations is:\n" + queryResult + ": " + myResult.getString(2));
+        }
+        catch(Exception e)
+        {
+            System.out.println("Error in getting Actions - Occupations table.");
+        }
+    }
 
+    public void SharedActionsOver30()
+    {
+        //Gives the table with the actions that is the most shared among occupations over level 30
+        try
+        {
+            String myQuery = "Select ActionName, COUNT(OccupationName) As SharedAmong From Perform Join Actions On Perform.ActionName = Actions.Name Where Level > 30 Group By ActionName Order By SharedAmong Desc;";
+            PreparedStatement myStatement = this.connection.prepareStatement(myQuery);
+            ResultSet myResult = myStatement.executeQuery();
+            PrintStream resultPrinter = System.out;
+            String queryResult = myResult.getString(1);
+            resultPrinter.println("The action that is shared by the most occupations over level 30 is:\n" + queryResult + ": " + myResult.getString(2));
+        }
+        catch(Exception e)
+        {
+            System.out.println("Error in getting Actions - Occupations over 30 table.");
+        }
+    }
+    public void CommonMonkAction()
+    {
+        //Gives the table with the most common Action type for Monk
+        try
+        {
+            String myQuery = "Select Type, Count(Actions.Type) As NumberOfActionTypes From Actions Group By Type Order By NumberOfActionTypes Desc;";
+            PreparedStatement myStatement = this.connection.prepareStatement(myQuery);
+            ResultSet myResult = myStatement.executeQuery();
+            PrintStream resultPrinter = System.out;
+            String queryResult = myResult.getString(1);
+            resultPrinter.println("The action type that is most common for Monk is:\n" + queryResult + ": " + myResult.getString(2));
+        }
+        catch(Exception e)
+        {
+            System.out.println("Error in getting Action Type for Monk.");
+        }
+    }
+    public void ActionMostQuest()
+    {
+        //Gives the table with the action type that is associated to the most number of quests
+        try
+        {
+            String myQuery = "Select Type, Count(Actions.questID) As NumberOfActions From Actions Group By Type Order By NumberOfActions Desc;";
+            PreparedStatement myStatement = this.connection.prepareStatement(myQuery);
+            ResultSet myResult = myStatement.executeQuery();
+            PrintStream resultPrinter = System.out;
+            String queryResult = myResult.getString(1);
+            resultPrinter.println("The action type that has the most quests associated is:\n" + queryResult + ": " + myResult.getString(2));
+        }
+        catch(Exception e)
+        {
+            System.out.println("Error in getting Action type with most number of quests.");
+        }
+    }
+    public void WindUpMinion()
+    {
+        //Gives the table with how many minions with the substring 'Wind up' are given from each acquisitionType
+        try
+        {
+            String myQuery = "Select AcquisitionType, count(Name) From Minions Where Name Like '%Wind-up%' Group By AcquisitionType Order By count(Name) Desc;";
+            PreparedStatement myStatement = this.connection.prepareStatement(myQuery);
+            ResultSet myResult = myStatement.executeQuery();
+            PrintStream resultPrinter = System.out;
+            String queryResult = myResult.getString(1);
+            resultPrinter.println("The number of minions with the substring 'Wind up' acquired from acquisitionType is:\n" + queryResult + ": " + myResult.getString(2));
+        }
+        catch(Exception e)
+        {
+            System.out.println("Error in windup minion query.");
+        }
+    }	
+    public void MountSpecialEffect()
+    {
+        //Gives the table with mounts that have special effects
+        try
+        {
+            String myQuery = "Select Name, Count(SpecialEffects) From Mounts Join MVEffect On Mounts.mountID = MVEffect.mountID Group By Name Having Count(SpecialEffects) > 0;";
+            PreparedStatement myStatement = this.connection.prepareStatement(myQuery);
+            ResultSet myResult = myStatement.executeQuery();
+            ResultSetMetaData rsmd = myResult.getMetaData();
+            int cols = rsmd.getColumnCount();
+            while (myResult.next())
+            {
+                for(int i = 1; i <= cols; i++)
+                {
+                    if(i > 1)
+                    {
+                        System.out.print(", ");
+                    }
+                    System.out.print(myResult.getString(i)); //print one element of a row
+                }
+                System.out.println("");
+            }
+
+            myResult.close();
+            myStatement.close();
+        }
+        catch(Exception e)
+        {
+            System.out.println("Error in getting table with special effect mounts.");
+        }
+    }	
+    public void AveRecastRoles()
+    {
+        //Gives the table with roles ranked based on their average recast time
+        try
+        {
+            String myQuery = "Select Role.Role, Avg(RecastTime) From Role Join Occupations on Occupations.Role = Role.Role Join Perform On Occupations.OccupationName = Perform.OccupationName Join Actions on Perform.ActionName = Actions.Name Group By Role.Role Order By Avg(RecastTime);";
+            PreparedStatement myStatement = this.connection.prepareStatement(myQuery);
+            ResultSet myResult = myStatement.executeQuery();
+            ResultSetMetaData rsmd = myResult.getMetaData();
+            int cols = rsmd.getColumnCount();
+            while (myResult.next())
+            {
+                for(int i = 1; i <= cols; i++)
+                {
+                    if(i > 1)
+                    {
+                        System.out.print(", ");
+                    }
+                    System.out.print(myResult.getString(i)); //print one element of a row
+                }
+                System.out.println("");
+            }
+
+            myResult.close();
+            myStatement.close();
+        }
+        catch(Exception e)
+        {
+            System.out.println("Error in ranking roles based on average recast time.");
+        }
+    }
     private void getTables(String tableName)
     {
         try
