5c2ed407ad1f07b72cb436abfe77e9d3469f1c8d
Agape Seo <96143174+gapy04@users.noreply.github.com>
2021-12-23 18:12:38 -0600

    Update MyFrontEnd.java

--- MyFrontEnd.java
+++ MyFrontEnd.java
@@ -68,8 +68,43 @@ public class MyFrontEnd
             }
             else if (var0[0].equals("oq"))
             {
-                //Gives the table with occupations and the number of quests each one ha
+                //Gives the table with occupations and the number of quests each one has
                 toRun.OccupationQuests();
+            }
+            else if (var0[0].equals("sa"))
+            {
+                //Gives the table with the actions that is the most shared among occupations
+                toRun.SharedActions();
+            }
+            else if (var0[0].equals("sa30"))
+            {
+                //Gives the table with the actions that is the most shared among occupations over level 30
+                toRun.SharedActionsOver30();
+            } 
+            else if (var0[0].equals("atMonk"))
+            {
+                //Gives the table with the most common Action type for Monk
+                toRun.CommonMonkAction();
+            }
+            else if (var0[0].equals("atQuests"))
+            {
+                //Gives the table with the action type that is associated to the most number of quests
+                toRun.ActionMostQuest();
+            }    
+            else if (var0[0].equals("WindupM"))
+            {
+                //Gives the table with how many minions with the substring 'Wind up' are given from each acquisitionType
+                toRun.WindUpMinion();
+            }
+            else if (var0[0].equals("seMounts"))
+            {
+                //Gives the table with mounts that have special effects
+                toRun.MountSpecialEffect();
+            }
+            else if (var0[0].equals("aveRecastRoles"))
+            {
+                //Gives the table with roles ranked based on their average recast time
+                toRun.AveRecastRoles();
             }
             else
             {
@@ -94,6 +129,13 @@ public class MyFrontEnd
         System.out.println("qt - Gives the table with quest givers and the variety of the type of quests they give");
         System.out.println("oa - Gives the table with occupations and number of actions each one has");
         System.out.println("oq - Gives the table with occupations and the number of quests each one has");
+        System.out.println("sa - Gives the table with the actions that is the most shared among occupations");
+        System.out.println("sa30 - Gives the table with the actions that is the most shared among occupations over level 30");
+        System.out.println("atMonk - Gives the table with the most common Action type for Monk");
+        System.out.println("atQuests - Gives the table with the action type that is associated to the most number of quests");
+        System.out.println("windupM - Gives the table with how many minions with the substring 'Wind up' are given from each acquisitionType");
+        System.out.println("seMounts - Gives the table with mounts that have special effects");
+        System.out.println("aveRecastRoles - Gives the table with roles ranked based on their average recast time");
 
         //system
         System.out.println("q - Exit the program");
